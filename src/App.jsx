1763e5262cd82bab57ba664143bd26e1aa23faf2
SamueleMaka <samuele.maka.imt@gmail.com>
2025-07-27 22:23:13 +0200

    feat: lista, funzionalità pulsanti e input

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,38 @@ function App() {
   const deleteArticle = (id) => {
     setArticles(articles.filter(currArticle => currArticle.id !== id));
   }
-
+  return (
+    <>
+      <h1>Aggiungi altri articoli</h1>
+      <div className="container">
+        <div className="articleContainer">
+          <ul>
+            {
+              articles.map(({id, titolo}) => {
+                return(
+                  <li key={id}>
+                    {titolo}
+                    <button
+                      onClick={() => deleteArticle(id)}
+                    >X</button>
+                  </li>
+                )
+                
+              })
+            }
+          </ul>
+        </div>
+        <div className="inputContainer">
+        <button onClick={newArticleHandler}>Aggiugni un nuovo articolo</button>
+          <input 
+            type="text"  
+            value={input}
+            onChange={(event)=>setInput(event.target.value)}
+          />
+      </div>
+      </div>
+    </>
+  )
 }
 
 export default App
