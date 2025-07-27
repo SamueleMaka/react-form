57c016544980016a271cf2b282302b181d53dc71
SamueleMaka <samuele.maka.imt@gmail.com>
2025-07-27 21:07:09 +0200

    feat: funzioni per aggiungere ed eliminare articoli

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import './App.css'
+import blogPosts from "./data/data.js";
+import { useState } from "react";
 
 function App() {
-  return (
-    <>
-      <div>Hello World!</div>
-    </>
-  )
+  const [input, setInput] = useState("");
+  const [articles, setArticles] = useState(blogPosts); 
+
+  const newArticleHandler = (event) => {
+    event.preventDefault();
+  
+    const newArticle = {
+      id: Date.now(),
+      titolo: input
+    };
+  
+    setArticles([...articles, newArticle]);
+  }
+
+  const deleteArticle = (id) => {
+    setArticles(articles.filter(currArticle => currArticle.id !== id));
+  }
+
 }
 
 export default App
